a815a5185dbf233166b190ece0f1045cee00847d
Alung12338 <64249260+Alung12338@users.noreply.github.com>
2020-04-28 01:43:32 +0000

    Add files via upload

---
+++ gabut.py
@@ -0,0 +1,135 @@
+# -- coding: utf-8 --
+import os
+import sys
+import time
+
+os.system('clear')
+print "\33[31;1m| |\     | |●   -------"
+print "\33[31;1m| | \    | | ●  |     |"
+print "\33[31;1m| |  \   | |  ) |     |"
+print "\33[37;1m| |   \  | |  ) |     |"
+print "\33[37;1m| |    \ | | ●  |     |"
+print "\33[37;1m| |     \| |●   -------"
+print "Loading..."
+time.sleep(3)
+os.system('clear')
+print "\33[31;1m《----------------------------------------》"
+print "\33[31;1m      Selamat menunaikan ibadah puasa"
+print "\33[37;1m《----------------------------------------》"
+print "harap sabar...."
+time.sleep(4)
+os.system('clear')
+print "\33[31;1m■■■■■■■■■■■■■■■■■■■■■■■"
+print "\33[31;1m■■■■■■■■■■■■■■■■■■■■■■■"
+print "\33[31;1m■■■■■■■■■■■■■■■■■■■■■■■"
+print "\33[37;1m■■■■■■■■■■■■■■■■■■■■■■■"
+print "\33[37;1m■■■■■■■■■■■■■■■■■■■■■■■"
+print "\33[37;1m■■■■■■■■■■■■■■■■■■■■■■■"
+print "sedang memuat....."
+time.sleep(4)
+os.system('clear')
+os.system('figlet Gabut')
+time.sleep(1)
+print "\33[1;33m<}<{<}<}<}<}<}<}<}<}<}<}<}<}<}<}<}<}<}<}<}"
+print "\33[1;33m Recode : Alung"
+print "\33[1;33m Youtube: Alung official"
+print "\33[1;33m Github : https://github.com/Alung12338"
+print "\33[1;33m<}<{<}<}<}<}<}<}<}<}<}<}<}<}<}<}<}<}<}<}<}"
+time.sleep(1)
+print 
+print "\33[31;1m[<>] menu pilihan [<>]"
+print "\33[31;1m[1] Phising-fb"
+print "\33[31;1m[2] jadwal buka puasa"
+print "\33[31;1m[3] ubah kuota ruangguru"
+print "\33[31;1m[4] Tool TUANBADUT"
+print "\33[31;1m[5] jangan di pilih"
+print "\33[31;1m[6] Teka teki"
+print "\33[31;1m[0] kembali"
+pilih = raw_input('[?]dipilih lur.. : ')
+if pilih == "1":
+        os.system('git clone https://github.com/Alung12338/Phising-Fb')
+        os.system('clear')
+        print "sucses.."
+        os.system('clear')
+        print "\33[0;36m=============================="
+        print "\33[0;36m untuk melanjutkan ketik :"
+        print "\33[0;36m $cd Phising-Fb (enter)"
+        print "\33[0;36m $ls (enter)"
+        print "\33[0;36m $python2 Alung.py (enter)"
+        print "\33[0;36m=============================="
+        time.sleep(1)
+        print "\33[31;1m!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+        print "\33[31;1m username : alung"
+        print "\33[31;1m password : fahrul"
+        print "\33[31;1m!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+        time.sleep(2)
+elif pilih == "2":
+        os.system('clear')
+        print "33[32;1m1) DKI Jakarta 17:52 WIB"
+        print "33[32;1m2) Bandung 17:47 WIB"
+        print "33[32;1m3) Semarang 17:32 WIB"
+        print "33[32;1m4) Yogyakarta 17:36 WIB"
+        print "33[32;1m5) Surabaya 17:27 WIB"
+        print "33[32;1m6) Makassar 18:02 WITA"
+        print "33[32;1m7) Manado 17:46 WITA"
+        print "33[32;1m8) Pontianak 17:47 WIB"
+        print "33[32;1m9) Banda Aceh 18:48 WIB"
+        print "33[32;1m10) Medan 18:33 WIB"
+        print "33[32;1m11) Padang 18:22 WIB"
+        print "33[32;1m12) Palembang 18:02 WIB"
+        print "33[32;1m13) Pekanbaru 18:19 WIB"
+        print "33[32;1m14) Mataram 18:12 WITA"
+        print "33[32;1m15) Denpasar 18:15 WITA"
+        print "33[32;1m16) Palu 18:04 WITA"
+        print "33[32;1m17) Ambon 18:28 WIT"
+        print "33[32;1m18) Jayapura 17:39 WIT"
+        os.system('figlet terimakasih')
+elif pilih == "3":
+        os.system('git clone https://github.com/Alung12338/IlmuNew')
+        print "selesai..."
+        time.sleep(2)
+        os.system('clear')
+        print "\33[32;1m□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□"
+        print "\33[32;1m untuk melanjutkan ketik :"
+        print "\33[32;1m $cd IlmuNew (enter)"
+        print "\33[32;1m $python2 main.py (enter)"
+        print "\33[32;1m□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□"
+elif pilih == "4":
+        os.system('git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3')
+        print "sucses.."
+        os.system('clear')
+        print "#cd TOOLSINSTALLERv3 (enter)"
+        print "#chmod +x TUANB4DUT.sh (enter)"
+        print "#sh TUANB4DUT.sh (enter)"
+elif pilih == "5":
+        os.system('clear')
+        print "\33[31;1mhp anda akan meledak"
+        time.sleep(2)
+        os.system('clear')
+        os.system('figlet 3')
+        time.sleep(3)
+        os.system('clear')
+        os.system('figlet 2')
+        time.sleep(3)
+        os.system('clear')
+        os.system('figlet 1')
+        time.sleep(2)
+        os.system('clear')
+        os.system('figlet 0')
+        time.sleep(1)
+        os.system('clear')
+        os.system('figlet Tapi Boong')
+        time.sleep(1)
+        os.system('python2 gabut.py')
+elif pilih == "6":
+        os.system('clear')
+        print "maaf muka anda jelek"
+        time.sleep(2)
+        os.system('clear')
+        print "Tidak dapat memuat"
+        time.sleep(2)
+        os.system('python2 gabut.py')
+elif pilih == "0":
+        print "Terima kasih telah menggunakan tools saya"
+        os.system('figlet bye')
+        time.sleep(2)
